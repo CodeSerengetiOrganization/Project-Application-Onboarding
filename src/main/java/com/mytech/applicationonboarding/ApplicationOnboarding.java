19439970c59e84682b770bb7a54d089149756337
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2024-09-10 18:45:00 -0400

    ticket1: add spring boot structure code; modified README.md.

---
+++ src/main/java/com/mytech/applicationonboarding/ApplicationOnboarding.java
@@ -0,0 +1,14 @@
+package com.mytech.applicationonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationOnboarding {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationOnboarding.class, args);
+		System.out.println("Application Onboarding System is running");
+	}
+
+}
